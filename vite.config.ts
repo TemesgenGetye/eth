538e1c015ce7a13dffe9ad6496b90469db28fc83
TemesgenGetye <temesgenselam1995@gmail.com>
2025-06-20 14:59:52 +0300

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      maxParallelFileOps: 100,
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['react-dom/client'],
   },
 });
